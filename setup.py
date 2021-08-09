9684aa9653d9f6ea708bbf9ead9c0f547ee6a8ad
Jamie <jdonaldmccann@gmail.com>
2021-08-09 22:32:35 +0100

    Add existing setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='matryoshka',
+      version='0.0.1',
+      description='Emulated halo model',
+      author='Jamie Donald-McCann',
+      author_email='jamie.donald-mccann@port.ac.uk',
+      packages=['matryoshka'],
+      zip_safe=False)
