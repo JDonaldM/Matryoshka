871e09c14d678794203f513289b427491f584313
minaskar <minaskar@gmail.com>
2021-09-29 10:05:19 +0100

    setup file updated

--- setup.py
+++ setup.py
@@ -1,9 +1,29 @@
-from setuptools import setup
+import setuptools
 
-setup(name='matryoshka',
-      version='0.0.3',
-      description='Emulated halo model',
-      author='Jamie Donald-McCann',
-      author_email='jamie.donald-mccann@port.ac.uk',
-      packages=['matryoshka'],
-      zip_safe=False)
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", 'r') as dependencies:
+    requirements = [pkg.strip() for pkg in dependencies]
+
+setuptools.setup(
+    name="matryoshka",
+    version="0.0.3",
+    author="Jamie Donald-McCann",
+    author_email="jamie.donald-mccann@port.ac.uk",
+    description="Emulated halo model",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/JDonaldM/Matryoshka",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        'Intended Audience :: Science/Research',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Mathematics',
+    ],
+    install_requires=requirements,
+    python_requires='>=3.6',
+)
