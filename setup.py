abc44f1bf054a0fb884ed43583bed97d2a1841cf
Jamie <jamie.donald-mccann@port.ac.uk>
2021-10-04 16:36:29 +0100

    Corrected license in setup file.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setuptools.setup(
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
         'Intended Audience :: Science/Research',
         'Topic :: Scientific/Engineering',
