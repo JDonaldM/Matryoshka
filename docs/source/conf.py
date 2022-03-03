2005f040570d5d909bde083ed8550648345326bb
Jamie <jamie.donald-mccann@port.ac.uk>
2022-03-03 14:51:40 +0000

    Calling this v0.2.8.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -23,7 +23,7 @@ copyright = '2021-2022, Jamie Donald-McCann'
 author = 'Jamie Donald-McCann'
 
 # The full version, including alpha/beta/rc tags
-release = '0.2.7'
+release = '0.2.8'
 
 
 # -- General configuration ---------------------------------------------------
