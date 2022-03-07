b313dc3db5206571dbb7483617499fc480cc145a
Jamie <jamie.donald-mccann@port.ac.uk>
2022-03-07 17:10:34 +0000

    Calling this v0.2.10

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open("requirements.txt", 'r') as dependencies:
 
 setuptools.setup(
     name="matryoshka",
-    version="0.2.9",
+    version="0.2.10",
     author="Jamie Donald-McCann",
     author_email="jamie.donald-mccann@port.ac.uk",
     description="Emulated halo model",
